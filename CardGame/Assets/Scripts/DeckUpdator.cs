b34f14f52eea9b9cc86e38d819c10eb55803655d
Horjamiini <54777254+Horjamiini@users.noreply.github.com>
2023-10-05 10:57:01 +0300

    CardGame_stuff

---
+++ CardGame/Assets/Scripts/DeckUpdator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckUpdator : MonoBehaviour
+{
+    List<int> Deck = new List<int>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        Deck.Add(1);
+        Deck.Add(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CardGame/Assets/Scripts/DrawCard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCard : MonoBehaviour
+{
+    RectTransform m_rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
